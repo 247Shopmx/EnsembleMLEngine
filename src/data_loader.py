288d935402fd497808684bd7810fe4fc10540d6e
247Shopmx <247Shop.mx@proton.me>
2026-06-27 11:29:57 -0600

    Update data_loader.py

--- src/data_loader.py
+++ src/data_loader.py
@@ -1,9 +1,14 @@
 import pandas as pd
 
-def load_csv(path):
-    data = pd.read_csv(path)
-    return data
+def load_matches():
+    df = pd.read_csv("../data/matches.csv")
+    return df
 
-def clean_data(df):
+def clean_matches(df):
     df = df.dropna()
+
+    df["home_win"] = (df["home_goals"] > df["away_goals"]).astype(int)
+    df["draw"] = (df["home_goals"] == df["away_goals"]).astype(int)
+    df["away_win"] = (df["home_goals"] < df["away_goals"]).astype(int)
+
     return df
