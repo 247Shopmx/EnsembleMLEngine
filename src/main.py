60fead7e3577339754cc1f3612743594549c9ddd
247Shopmx <247Shop.mx@proton.me>
2026-06-27 11:31:25 -0600

    Update main.py

--- src/main.py
+++ src/main.py
@@ -1,13 +1,13 @@
-import json
-
-def load_config():
-    with open("../config/config.json", "r") as f:
-        return json.load(f)
+from data_loader import load_matches, clean_matches
+from features import build_features
 
 def main():
-    config = load_config()
-    print("Sistema Football AI iniciado")
-    print("Modelo activo:", config["models"])
+    df = load_matches()
+    df = clean_matches(df)
+    df = build_features(df)
+
+    print("Dataset listo para ML")
+    print(df.head())
 
 if __name__ == "__main__":
     main()
