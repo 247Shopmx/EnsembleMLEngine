517b686533c5a9ab0c6d09b8b48cf1882e82ceb8
247Shopmx <247Shop.mx@proton.me>
2026-06-27 11:32:42 -0600

    Update main.py

--- src/main.py
+++ src/main.py
@@ -1,13 +1,17 @@
 from data_loader import load_matches, clean_matches
 from features import build_features
+from model_xgboost import train_model
 
 def main():
     df = load_matches()
     df = clean_matches(df)
     df = build_features(df)
 
-    print("Dataset listo para ML")
-    print(df.head())
+    print("Dataset listo:", df.shape)
+
+    model = train_model(df)
+
+    print("Modelo entrenado correctamente")
 
 if __name__ == "__main__":
     main()
